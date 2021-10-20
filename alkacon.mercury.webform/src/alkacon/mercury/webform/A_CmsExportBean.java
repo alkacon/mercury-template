8a7d1fae3dc8812a47a9af175fa55cf70a70aa63
jGraf <j.graf@alkacon.com>
2021-10-20 14:52:51 +0200

    Webform: Added rename / merge field mechanism to form data export.

--- alkacon.mercury.webform/src/alkacon/mercury/webform/A_CmsExportBean.java
+++ alkacon.mercury.webform/src/alkacon/mercury/webform/A_CmsExportBean.java
@@ -135,6 +135,9 @@ public abstract class A_CmsExportBean extends A_CmsJspCustomContextBean {
     /** Configuration node name for the value. */
     public static final String NODE_EXPORT_IGNORE_FORMER_FIELD = "IgnoreFormerField";
 
+    /** Configuration node name for the value. */
+    public static final String NODE_EXPORT_RENAME_FIELD = "RenameField";
+
     /** List of fields to ignore during export. */
     private List<String> m_exportConfigFieldIgnore = new ArrayList<String>();
 
@@ -167,22 +170,8 @@ public abstract class A_CmsExportBean extends A_CmsJspCustomContextBean {
         m_form = form;
         m_formTitle = formTitle;
         m_messages = new CmsMessages(BUNDLE_NAME, locale);
-        CmsObject cms = getCmsObject();
-        if (m_form.getFormConfig().hasValue(NODE_EXPORT, locale)) {
-            String pathPrefix = NODE_EXPORT + "/" + NODE_EXPORT_IGNORE_FIELD;
-            if (m_form.getFormConfig().hasValue(pathPrefix, locale)) {
-                String pathIgnoreInputField = pathPrefix + "/" + NODE_EXPORT_IGNORE_INPUT_FIELD;
-                String pathIgnoreDependentField = pathPrefix + "/" + NODE_EXPORT_IGNORE_DEPENDENT_FIELD;
-                String pathIgnoreFormerField = pathPrefix + "/" + NODE_EXPORT_IGNORE_FORMER_FIELD;
-                List<I_CmsXmlContentValue> values = new ArrayList<I_CmsXmlContentValue>();
-                values.addAll(m_form.getFormConfig().getValues(pathIgnoreInputField, locale));
-                values.addAll(m_form.getFormConfig().getValues(pathIgnoreDependentField, locale));
-                values.addAll(m_form.getFormConfig().getValues(pathIgnoreFormerField, locale));
-                for (I_CmsXmlContentValue value : values) {
-                    m_exportConfigFieldIgnore.add(value.getStringValue(cms));
-                }
-            }
-        }
+        initExportConfigFieldIgnore(locale);
+        initExportConfigFieldRename(locale);
     }
 
     /**
@@ -209,6 +198,9 @@ public abstract class A_CmsExportBean extends A_CmsJspCustomContextBean {
             for (String field : formDataBean.getData().keySet()) {
                 if (m_exportConfigFieldIgnore.contains(field)) {
                     continue;
+                }
+                if (m_exportConfigFieldRename.containsKey(field)) {
+                    field = m_exportConfigFieldRename.get(field);
                 }
                 if (!columnNames.containsKey(field)) {
                     columnNames.put(field, Integer.valueOf(columnNames.size()));
@@ -237,6 +229,7 @@ public abstract class A_CmsExportBean extends A_CmsJspCustomContextBean {
 
     /**
      * Returns the data of one submission as a map. May include inactive former columns.
+     * Filters fields to ignore. Renames and merges fields according to the export configuration.
      * @param formData the submission data as bean.
      * @return the submission data as map.
      */
@@ -244,17 +237,28 @@ public abstract class A_CmsExportBean extends A_CmsJspCustomContextBean {
 
         Map<String, String> data = formData.getData();
         Iterator<Map.Entry<String, String>> iter = data.entrySet().iterator();
+        Map<String, String> merged = new LinkedHashMap<String, String>();
         while (iter.hasNext()) {
             Map.Entry<String, String> entry = iter.next();
-            if (m_exportConfigFieldIgnore.contains(entry.getKey())) {
-                iter.remove();
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (m_exportConfigFieldIgnore.contains(key)) {
+                continue;
+            }
+            if (m_exportConfigFieldRename.containsKey(key)) {
+                key = m_exportConfigFieldRename.get(key);
+            }
+            if (merged.containsKey(key)) {
+                merged.put(key, merged.get(key) + " " + value);
+            } else {
+                merged.put(key, value);
             }
         }
-        data.put(m_messages.key(KEY_STATUS_CANCELLED), asString(formData.isCancelled()));
-        data.put(m_messages.key(KEY_STATUS_WAITLIST), asString(formData.isWaitlist()));
-        data.put(m_messages.key(KEY_STATUS_CONFIRMED), asString(formData.isConfirmationMailSent()));
-        data.put(m_messages.key(KEY_STATUS_CHANGED), asString(formData.isChanged()));
-        return data;
+        merged.put(m_messages.key(KEY_STATUS_CANCELLED), asString(formData.isCancelled()));
+        merged.put(m_messages.key(KEY_STATUS_WAITLIST), asString(formData.isWaitlist()));
+        merged.put(m_messages.key(KEY_STATUS_CONFIRMED), asString(formData.isConfirmationMailSent()));
+        merged.put(m_messages.key(KEY_STATUS_CHANGED), asString(formData.isChanged()));
+        return merged;
     }
 
     /**
@@ -278,6 +282,51 @@ public abstract class A_CmsExportBean extends A_CmsJspCustomContextBean {
             }
         }
         return formDataBeans;
+    }
+
+    /**
+     * Reads the ignore field export configuration from the form content.
+     * @param locale the locale
+     */
+    private void initExportConfigFieldIgnore(Locale locale) {
+
+        CmsObject cms = getCmsObject();
+        if (m_form.getFormConfig().hasValue(NODE_EXPORT, locale)) {
+            String pathPrefix = NODE_EXPORT + "/" + NODE_EXPORT_IGNORE_FIELD;
+            if (m_form.getFormConfig().hasValue(pathPrefix, locale)) {
+                String pathIgnoreInputField = pathPrefix + "/" + NODE_EXPORT_IGNORE_INPUT_FIELD;
+                String pathIgnoreDependentField = pathPrefix + "/" + NODE_EXPORT_IGNORE_DEPENDENT_FIELD;
+                String pathIgnoreFormerField = pathPrefix + "/" + NODE_EXPORT_IGNORE_FORMER_FIELD;
+                List<I_CmsXmlContentValue> values = new ArrayList<I_CmsXmlContentValue>();
+                values.addAll(m_form.getFormConfig().getValues(pathIgnoreInputField, locale));
+                values.addAll(m_form.getFormConfig().getValues(pathIgnoreDependentField, locale));
+                values.addAll(m_form.getFormConfig().getValues(pathIgnoreFormerField, locale));
+                for (I_CmsXmlContentValue value : values) {
+                    m_exportConfigFieldIgnore.add(value.getStringValue(cms));
+                }
+            }
+        }
+    }
+
+    /**
+     * Reads the rename field export configuration from the form content.
+     * @param locale the locale
+     */
+    private void initExportConfigFieldRename(Locale locale) {
+
+        CmsObject cms = getCmsObject();
+        if (m_form.getFormConfig().hasValue(NODE_EXPORT, locale)) {
+            String pathPrefix = NODE_EXPORT + "/" + NODE_EXPORT_RENAME_FIELD;
+            int numRenameFields = m_form.getFormConfig().getIndexCount(pathPrefix, locale);
+            for (int i = 1; i <= numRenameFields; i++) {
+                String pathRenameFieldOrig = pathPrefix + "[" + i + "]/RenameFieldOrig";
+                String pathRenameFieldNew = pathPrefix + "[" + i + "]/RenameFieldNew";
+                String renameFieldOrig = m_form.getFormConfig().getValue(pathRenameFieldOrig, locale).getStringValue(
+                    cms);
+                String renameFieldNew = m_form.getFormConfig().getValue(pathRenameFieldNew, locale).getStringValue(cms);
+                m_exportConfigFieldRename.put(renameFieldOrig, renameFieldNew);
+            }
+        }
     }
 
     /**

05f71dc657fffadce58fef81be908759d1c5aba5
jGraf <j.graf@alkacon.com>
2022-01-14 16:11:01 +0100

    Geo search: First version of map with marker clustering.

--- template-src/js/map-osm.js
+++ template-src/js/map-osm.js
@@ -158,27 +158,100 @@ export function showMarkers(mapId, group){
     }
 }
 
-export function updateMarkers(mapId, mapData) {
+export function showGeoJson(mapId, mapData) {
 
     if (DEBUG) console.info("OSM update markers for map with id: " + mapId);
-    let mapDataIndex;
-    for (let i = 0; i < m_mapData.length; i++) {
-        if (m_mapData[i].id === mapId) {
-            mapDataIndex = i;
-            break;
-        }
-    }
-    const mapDataOrig = m_mapData[mapDataIndex];
-    if (!mapDataOrig) {
-        console.error("Error when updating markers. Map with id " + mapId + " not found.");
+    const map = m_maps[mapId];
+    if (!map) { // no cookie consent yet
         return;
     }
-    mapData.id = mapId;
-    mapData.showPlaceholder = mapDataOrig.showPlaceholder;
-    m_mapData[mapDataIndex] = mapData;
-    if (!mapData.showPlaceholder) {
-        showSingleMap(mapData);
-    }
+    map.addSource('features', {
+        type: 'geojson',
+        data: mapData,
+        cluster: true,
+        clusterMaxZoom: 14,
+        clusterRadius: 50
+    });
+    map.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'features',
+        filter: ['has', 'point_count'],
+        paint: {
+            'circle-color': '#51bbd6',
+            'circle-radius': 20,
+            'circle-stroke-width': 2,
+            'circle-stroke-color': '#fff'
+        }
+    });
+    map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'features',
+        filter: ['has', 'point_count'],
+        layout: {
+            'text-field': '{point_count_abbreviated}',
+            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+            'text-size': 12
+        }
+    });
+    map.addLayer({
+        id: 'unclustered-point',
+        type: 'circle',
+        source: 'features',
+        filter: ['!', ['has', 'point_count']],
+        paint: {
+            'circle-color': '#11b4da',
+            'circle-radius': 10,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#fff'
+        }
+    });
+    map.on('click', 'clusters', function (e) {
+        var features = map.queryRenderedFeatures(e.point, {
+            layers: ['clusters']
+        });
+        var clusterId = features[0].properties.cluster_id;
+        map.getSource('features').getClusterExpansionZoom(
+            clusterId,
+            function (err, zoom) {
+                if (err) return;
+                map.easeTo({
+                    center: features[0].geometry.coordinates,
+                    zoom: zoom
+                });
+            }
+        );
+    });
+    map.on('click', 'unclustered-point', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var mag = e.features[0].properties.mag;
+        var tsunami;
+        if (e.features[0].properties.tsunami === 1) {
+            tsunami = 'yes';
+        } else {
+            tsunami = 'no';
+        }
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        new mapgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML('magnitude: ' + mag + '<br>Was there a tsunami?: ' + tsunami)
+            .addTo(map);
+    });
+    map.on('mouseenter', 'clusters', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    map.on('mouseleave', 'clusters', function () {
+        map.getCanvas().style.cursor = '';
+    });
+    map.on('mouseenter', 'unclustered-point', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    map.on('mouseleave', 'unclustered-point', function () {
+        map.getCanvas().style.cursor = '';
+    });
 }
 
 function showMap(event){
